0a225de0aed4d95194af93bda85452999a89d139
bmgwaro <brian.gwaro@student.moringaschool.com>
2024-07-15 11:58:19 +0300

    liker app lab

--- main.js
+++ main.js
@@ -23,3 +23,38 @@ function mimicServerCall(url="http://mimicServer.example.com", config={}) {
     }, 300);
   });
 }
+
+// Add the .hidden class to the error modal
+const errorModal = document.getElementById('modal');
+errorModal.classList.add('hidden');
+
+// Function to handle heart click
+function handleHeartClick(event) {
+  const heart = event.target;
+
+  // Simulate server call
+  mimicServerCall()
+    .then(() => {
+      // On success, toggle the heart state
+      if (heart.innerText === EMPTY_HEART) {
+        heart.innerText = FULL_HEART;
+        heart.classList.add('activated-heart');
+      } else {
+        heart.innerText = EMPTY_HEART;
+        heart.classList.remove('activated-heart');
+      }
+    })
+    .catch((error) => {
+      // On failure, display error modal
+      errorModal.classList.remove('hidden');
+      errorModal.querySelector('#modal-message').innerText = error;
+      setTimeout(() => {
+        errorModal.classList.add('hidden');
+      }, 3000);
+    });
+}
+
+// Add event listeners to all hearts
+document.querySelectorAll('.like-glyph').forEach(heart => {
+  heart.addEventListener('click', handleHeartClick);
+});
